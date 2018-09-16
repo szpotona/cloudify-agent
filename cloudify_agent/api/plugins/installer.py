af18c13cf15980d06cca6adc52890dd57989f90c
Anna Szpoton <anna.szpoton@cloudify.co>
2018-09-16 22:07:55 +0200

    Minimum changes

--- cloudify_agent/api/plugins/installer.py
+++ cloudify_agent/api/plugins/installer.py
@@ -20,7 +20,6 @@ import shutil
 import tempfile
 import platform
 import logging
-import fcntl
 
 from distutils.version import LooseVersion
 
@@ -249,13 +248,7 @@ class PluginInstaller(object):
                 plugin_dir = source
             else:
                 self.logger.debug('Extracting archive: {0}'.format(source))
-                with open('/tmp/plugin-installer.lock', 'w') as lock_fd:
-                    fcntl.lockf(lock_fd, fcntl.LOCK_EX)
-                    try:
-                        plugin_dir = extract_package_to_dir(source)
-                    finally:
-                        fcntl.lockf(lock_fd, fcntl.LOCK_UN)
-                self.logger.debug('Extracted to {}'.format(plugin_dir))
+                plugin_dir = extract_package_to_dir(source)
             package_name = extract_package_name(plugin_dir)
             if self._package_installed_in_agent_env(constraint, package_name):
                 self.logger.warn('Skipping source plugin {0} installation, '
